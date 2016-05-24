fdd7a379349cf6dd57f716e7fffb0077e906f989
Frederica Caira <Freddie@Timmy-the-MacBook-Pro.local>
2016-05-24 23:49:26 +0100

    HTML commented & cleaned

--- site/FileStructure.md
+++ site/FileStructure.md
@@ -6,9 +6,9 @@ This is loosely based on the Express application generator framework.
 
 + `node_modules/` – contains libraries used by node.js
 + `public/` – contains all static files like images, styles and javascript
-- `images/` - contains static images, e.g. background
-- `javascripts/` - contains all front end javascript
-- `stylesheets/` - contains all CSS files
+	+ `images/` - contains static images, e.g. background
+	+ `javascripts/` - contains all front end javascript
+	+ `stylesheets/` - contains all CSS files
 + `routes/` – defines your app routes and their logic, i.e. backend javascript etc.
 + `views/` – provides templates which are rendered and served by your routes, i.e. HTML files
 + `server.js` – initializes the app and glues everything together
---
+++ site/public/javascripts/controller.js
@@ -0,0 +1,51 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+	console.log("Controller listening");
+
+	var refresh = function() {
+		$http.get('/commentlist').success(function(response) {
+			console.log("I got the data I requested");
+			$scope.commentlist = response;
+			$scope.comment = "";
+		});
+	};
+
+	refresh();
+
+	var today = new Date();
+	var hr = today.getHours();
+	var min = today.getMinutes();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1;
+	var yyyy = today.getFullYear();
+	if (dd < 10) {
+		dd = '0' + dd
+	}
+	if (mm < 10) {
+		mm = '0' + mm
+	}
+	var today = hr + ':' + min + ' ' + dd + '/' + mm + '/' + yyyy;
+
+	$scope.addComment = function() {
+		$scope.comment.datetime = today;
+		console.log($scope.comment);
+		$http.post('/commentlist', $scope.comment).success(function(response) {
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.deselect = function() {
+		$scope.comment = "";
+	}
+
+}]);﻿
+
+myApp.filter('reverse', function() {
+	return function(items) {
+		if (!items || !items.length) {
+			return;
+		}
+		return items.slice().reverse();
+	};
+});

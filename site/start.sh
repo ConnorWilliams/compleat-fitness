704771aa8986a9d39824e471edaade8a4b155b8d
Connor <connor_williams@msn.com>
2016-05-28 11:03:30 +0100

    changed an incorrect menu image on contact page

--- site/start.sh
+++ site/start.sh
@@ -3,4 +3,4 @@
 # - creates directory for database if doesn't already exist
 # - starts host process for databases 
 # - starts the server
-mkdir data & mongod -dbpath data & node appneg.js
+mkdir data & mongod -dbpath data & nodejs appneg.js

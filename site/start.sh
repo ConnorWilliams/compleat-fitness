7fa160ad7a977693956e91470a74ff0a78ec146c
Frederica Caira <Freddie@Timmy-the-MacBook-Pro.local>
2016-05-28 22:43:43 +0100

    Some updates to report made, more to be done

--- site/start.sh
+++ site/start.sh
@@ -3,4 +3,4 @@
 # - creates directory for database if doesn't already exist
 # - starts host process for databases 
 # - starts the server
-mkdir data & mongod -dbpath data & nodejs server.js
+mkdir data & mongod -dbpath data & node server.js
